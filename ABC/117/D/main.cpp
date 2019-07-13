0d6b6a699e44ca83b6bb79c62f1a43937dbd4123
[kraquos] <[kregistaa@gmail.com]>
2019-07-13 10:50:34 +0900

    ABC/117/D

--- ABC/117/D/main.cpp
+++ ABC/117/D/main.cpp
@@ -8,30 +8,77 @@
 #define MAX 1000000007
 using namespace std;
 
+ll cnt[45][2],kbit[45];
+
+ll calc(int x){
+	ll ans=0,pow2=1;
+	rep(i,x){
+		if(cnt[i][0]>cnt[i][1])ans+=pow2*cnt[i][0];
+		else ans+=pow2*cnt[i][1];
+		pow2*=2;
+	}
+	return ans;
+}
+
 int main(void){
 	ll n,k;
 	cin>>n>>k;
-	ll a[n], max=0;
-	rep(i,n){
-		cin>>a[i];
-		if(a[i]>max)max=a[i];
-	}
-	ll bit=2,id=1;
-	while(bit<=k){
-		id++;
-		bit*=2;
-	}
-	for(i=0;i<100;i++){
-		rep(j,n){
-			if(a[j]%2==1){
-				cnt1++;
-				w[i][1]-=1;
-			}
-			else w[i][0]++;
-			a[j]/=2;
-		}
+	ll a[n];
+	rep(i,n)cin>>a[i];
 
+	ll pow2=1,pow=1;
+	rep(i,42){
+		pow2*=2;
+		pow*=2;
+		cnt[i][0]=0;
+		cnt[i][1]=0;
+		kbit[i]=0;
 	}
+
+	int idx=42;
+	while(idx > -1){
+		rep(i,n){
+			if(a[i] >= pow2){
+				cnt[idx][1]++;
+				a[i]-=pow2;
+			}
+			else cnt[idx][0]++;
+			if(k >= pow2){
+				kbit[idx]=1;
+				k-=pow2;
+			}
+		}
+		pow2/=2;
+		idx--;
+	}
+
+	/*
+	rep(i,5)cout<<cnt[i][0];
 	cout<<endl;
+	rep(i,5)cout<<cnt[i][1];
+	cout<<endl;
+	rep(i,5)cout<<kbit[i];
+	cout<<endl;*/
+
+	ll val=0,maxf=0;
+	for(int i=42;i>-1;i--){
+		//cout<<"i:"<<i<<","<<"k:"<<kbit[i]<<","<<"c[0]:"<<cnt[i][0]<<","<<"c[1]:"<<cnt[i][1]<<endl;
+		if(kbit[i]){
+			if(cnt[i][1] >= cnt[i][0]){
+				val+=cnt[i][1]*pow+calc(i);
+				break;
+			}
+			ll tmp=calc(i);
+			if(maxf < val+cnt[i][1]*pow+tmp)maxf=val+cnt[i][1]*pow+tmp;
+			val+=cnt[i][0]*pow;
+		}
+		else val+=cnt[i][1]*pow;
+		pow/=2;
+	}
+
+	if(val > maxf)maxf=val;
+
+	cout<<maxf<<endl;
+
 	return 0;
 }
